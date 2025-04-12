64801741c6719757ebb9b7fa06276684d2665fd1
H.Humpel <H.Humpel@gmx.de>
2025-04-12 15:21:20 +0200

    [NEW] 3448638278 : 深空之眼 苍鹭·托特

---
+++ Originals/3448638278/TEXT_AETHERGAZER_EXPANSION_PART_TUOTE.sql
@@ -0,0 +1,87 @@
+-- TEXT_AETHERGAZER_EXPANSION_PART_TUOTE
+-- Author: HK433
+-- DateCreated: 10/31/2024 9:17:42 PM
+--------------------------------------------------------------
+INSERT OR REPLACE INTO LocalizedText(Tag,Language,Text)
+VALUES
+('LOC_CIVILIZATION_AETHERGAZER_YELLOW_TUOTE_NAME','zh_Hans_CN','尼罗神域-苍鹭'),
+('LOC_CIVILIZATION_AETHERGAZER_YELLOW_TUOTE_CAPITALNAME','zh_Hans_CN','神域·尼罗'),
+--领袖名字
+('LOC_LEADER_TUOTE_AETHERGAZER_NAME','zh_Hans_CN','托特'),
+--托特
+('LOC_PEDIA_LEADERS_PAGE_LEADER_TUOTE_AETHERGAZER_QUOTE','zh_Hans_CN','凡事过往，皆为序曲'),
+('LOC_TRAIT_LEADER_TUOTE_AETHERGAZER_NAME','zh_Hans_CN','丝与刃'),
+('LOC_TRAIT_LEADER_TUOTE_AETHERGAZER_DESCRIPTION','zh_Hans_CN','创建首都后立即获得托特、奥西里斯和获得1个间谍（并增加1个间谍容量），工作坊+1间谍容量，间谍+14[ICON_Gold]金币。进攻性间谍任务成功时，获得目标城市该回合[ICON_Science]科技值和[ICON_Culture]文化值的1000%，完成所有任务的时间减少100%且部署在敌人城市无需消耗时间，进行反间谍时效果将覆盖所有城市区域且提升100级。开拓者、建造者和间谍购买花费-95%。每完成一个科技和市政时，托特视野范围+1'),
+('LOC_LOADING_INFO_LEADER_TUOTE_AETHERGAZER','zh_Hans_CN','同样的事物在不同人的眼中会映出不同的样子，对某些人来说科尔盖或许意味着正义与秩序.但对我来说......科尔盖意味着牺牲与苦涩的选择——托特'),
+('LOC_AGENDA_TUOTE_NAME','zh_Hans_CN','收敛'),
+('LOC_AGENDA_TUOTE_DESCRIPTION','zh_Hans_CN','喜欢军事力量较为薄弱的文明'),
+('LOC_ABILITY_TUOTE_14_GOLD_FOR_SPY_NAME','zh_Hans_CN','单位+14[ICON_Gold]金币'),
+--托特外交
+--同意蓝脸
+('LOC_DIPLO_ACCEPT_DECLARE_FRIEND_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','（点头）我早有准备，接着说吧'),
+--拒绝蓝脸
+('LOC_DIPLO_REJECT_DECLARE_FRIEND_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','恕我拒绝'),
+--请求蓝脸
+('LOC_DIPLO_DECLARE_FRIEND_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','很期待与你合作'),
+--人类接受结盟
+('LOC_DIPLO_ACCEPT_DECLARE_FRIEND_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','别担心，一切都已经计划好了'),
+--人类拒绝结盟
+('LOC_DIPLO_REJECT_DECLARE_FRIEND_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','没关系，无所谓'),
+--被宣
+('LOC_DIPLO_DECLARE_WAR_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','不速之客只在退场以后才最受欢迎'),
+--主宣
+('LOC_DIPLO_DECLARE_WAR_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','生存还是毁灭，你们不必再为之思考'),
+--接受使者
+('LOC_DIPLO_ACCEPT_DELEGATION_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','派来的使者我已经安顿好了，请放心'),
+--拒绝使者
+('LOC_DIPLO_REJECT_DELEGATION_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','科尔盖向来不接纳任何闲杂人员'),
+--向人类派遣使者
+('LOC_DIPLO_DELEGATION_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','科尔盖谈判专家就快要到了，希望你能好好接待他们，这样对你也有好处'),
+--被谴责
+('LOC_DIPLO_DENOUNCE_FROM_HUMAN_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','哼，随便你'),
+--谴责人类
+('LOC_DIPLO_DENOUNCE_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','垃圾里是淘不出金子来的'),
+--遇见人类
+('LOC_DIPLO_FIRST_MEET_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','哦？一位可爱的伙伴加入了漫长的旅途...我是科尔盖重大犯罪对策部，托特。别紧张，我知道你也不想经常见到我，对吧？'),
+--遇见人类，提供最近城市位置
+('LOC_DIPLO_FIRST_MEET_VISIT_RECIPIENT_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','要来艾因索菲参观一下嘛，离这儿不是很远，你应该不会拒绝吧？'),
+--遇见人类，玩家提供位置
+('LOC_DIPLO_FIRST_MEET_NEAR_INITIATOR_POSITIVE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','嗯，正好我也有空'),
+--遇见人类，互相交换位置
+('LOC_DIPLO_FIRST_MEET_NO_MANS_INFO_EXCHANGE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','你说的这个位置我从来都没听说过，听上去离这儿挺远，要不在地图上标记一下你们位置吧，我也会告诉你我们的位置在哪儿'),
+--请求结盟
+-- ('LOC_DIPLO_MAKE_ALLIANCE_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','NULL'),
+--警告玩家边境部队太多
+('LOC_DIPLO_WARNING_TOO_MANY_TROOPS_NEAR_ME_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','收起你的锋芒，这样对双方都有好处'),
+--玩家同意撤走
+('LOC_DIPLO_WARNING_TOO_MANY_TROOPS_NEAR_ME_HUMAN_RESPONSE_POSITIVE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','趁科尔盖对你们的耐心还没耗尽之前，赶紧离开这里'),
+--警告玩家不要在我周边建城
+('LOC_DIPLO_WARNING_DONT_SETTLE_NEAR_ME_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','你们的据点已经影响到科尔盖的地盘了'),
+--玩家拒绝建城
+('LOC_DIPLO_WARNING_DONT_SETTLE_NEAR_ME_HUMAN_RESPONSE_NEGATIVE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','你可以继续这样的行为，后果自负'),
+--警告玩家间谍在AI家里
+('LOC_DIPLO_WARNING_STOP_SPYING_ON_ME_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','想在科尔盖安插眼线可没那么容易'),
+--玩家拒绝间谍
+('LOC_DIPLO_WARNING_STOP_SPYING_ON_ME_HUMAN_RESPONSE_NEGATIVE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','那你可以再试试'),
+--警告玩家不允许传教
+('LOC_DIPLO_WARNING_STOP_CONVERTING_MY_CITIES_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','你能把你那些歪理邪说的家伙召回去嘛？'),
+--玩家继续传教
+('LOC_DIPLO_WARNING_STOP_CONVERTING_MY_CITIES_HUMAN_RESPONSE_NEGATIVE_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','无话可说'),
+--战败
+('LOC_DIPLO_DEFEAT_FROM_AI_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','这就是...故事的结局吗'),
+--同意交易
+('LOC_DIPLO_ACCEPT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_NEUTRAL','zh_Hans_CN','合作很愉快，谢谢'),
+('LOC_DIPLO_ACCEPT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_HAPPY','zh_Hans_CN','合作很愉快，谢谢'),
+('LOC_DIPLO_ACCEPT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_UNHAPPY','zh_Hans_CN','合作很愉快，谢谢'),
+--拒绝交易
+('LOC_DIPLO_REJECT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_NEUTRAL','zh_Hans_CN','这都不能打动你的心？'),
+('LOC_DIPLO_REJECT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_HAPPY','zh_Hans_CN','这都不能打动你的心？'),
+('LOC_DIPLO_REJECT_MAKE_DEAL_FROM_AI_LEADER_TUOTE_AETHERGAZER_UNHAPPY','zh_Hans_CN','这都不能打动你的心？'),
+--议程好感
+('LOC_DIPLO_KUDO_EXIT_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN','强大的武力有时对别人造成威胁，收敛一些大家都好，这一点上你做的很不错'),
+--议程厌恶,没有
+-- ('LOC_DIPLO_WARNING_EXIT_LEADER_TUOTE_AETHERGAZER_ANY','zh_Hans_CN',''),
+--议程好感显示
+('LOC_DIPLO_MODIFIER_DESCRIPTION_AGENDA_TUOTE_HAPPY','zh_Hans_CN','托特非常开心看到你没发展军事力量');
+--议程厌恶显示，没有
+-- ('LOC_DIPLO_MODIFIER_DESCRIPTION_AGENDA_TUOTE_SAD','zh_Hans_CN',''),
--- README.md
+++ README.md
@@ -304,6 +304,7 @@ Auto-generated translations for the following mods are currently included in thi
 | [3440390953](https://steamcommunity.com/sharedfiles/filedetails/?id=3440390953) | civ 5 new leaders | civ 5 new leaders |
 | [3447024901](https://steamcommunity.com/sharedfiles/filedetails/?id=3447024901) | KiloWhite's MOD | KiloWhite's MOD |
 | [3448635771](https://steamcommunity.com/sharedfiles/filedetails/?id=3448635771) | 深空之眼 核心代码 阶段2 | Eye of Deep Space Core Code Stage 2 |
+| [3448638278](https://steamcommunity.com/sharedfiles/filedetails/?id=3448638278) | 深空之眼 苍鹭·托特 | Eye of the Deep Space Heron Tot |
 | [3451186356](https://steamcommunity.com/sharedfiles/filedetails/?id=3451186356) | [HONKAI:STAR RAIL] AMPHOREUS PACK | [HONKAI:STAR RAIL] AMPHOREUS PACK |
 | [3455609077](https://steamcommunity.com/sharedfiles/filedetails/?id=3455609077) | [HONKAI:STAR RAIL] AMPHOREUS PLUS PACK | [HONKAI:STAR RAIL] AMPHOREUS PLUS PACK |
 | [3458385576](https://steamcommunity.com/sharedfiles/filedetails/?id=3458385576) | 백귀야행 연합학원 | Backwi Nocturnal Academy |
